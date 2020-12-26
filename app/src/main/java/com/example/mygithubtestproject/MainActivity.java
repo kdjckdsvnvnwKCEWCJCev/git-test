d953ec299caa94d735caeebcb6686167b6ea62fb
unknown <helsifn@gmail.com>
2020-12-26 18:20:12 +0300

    kek'

--- app/src/main/java/com/example/mygithubtestproject/MainActivity.java
+++ app/src/main/java/com/example/mygithubtestproject/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// АХАХАХАХАХАХАХАХАХАХАХХААХА ЛОЛ ТЕБЯ ВЗЛОМАЛИ И ТЕПЕРЬ ПЛАТИ ДЕНГИ МНЕ НА НОМЕР 880055355!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
